9b339f2d690b1b27061c58c9898f24b0e1531be9
Louis Cloutier <louisphp@gmail.com>
2015-10-18 23:46:06 -0400

    Add basic hello world with hot-reload.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # what-to-steam
 Decide what game to steam.
+
+#How to start
+'npm install'
+'npm start'
+
+#Linting
+For linting we are using eslint. We use the excellent [airbnb config](https://github.com/airbnb/javascript/tree/master/packages/eslint-config-airbnb)airbnb config as a base
+To work on Sublime text 3. You need to install two plugin Sublimelinter and SublimeLinter-contrib-eslint
---
+++ webpack.config.dev.js
@@ -0,0 +1,26 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  devtool: 'source-map',
+  entry: [
+    'webpack-hot-middleware/client',
+    './app/js/index'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/js/'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loaders: ['babel'],
+      include: path.join(__dirname, 'app/js')
+    }]
+  }
+};
