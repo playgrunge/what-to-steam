222e92c688f39c7ca3ba9dacb218cb9c39bb3cca
Louis Cloutier <louisphp@gmail.com>
2015-11-01 08:46:51 -0500

    Add initial config for gh-pages

--- app/js/config/config.prod.js
+++
@@ -1,1 +0,0 @@
-export const STEAM_KEY = 'STEAM_KEY';
--- app/js/reducers/MainReducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import games from './GameReducer';
-
-const MainReducer = combineReducers({
-    games
-});
-
-export default MainReducer;
